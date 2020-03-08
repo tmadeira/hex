a33a5433f708d9857559f814a968909009d4d466
Tiago Madeira <tmadeira@gmail.com>
2020-03-08 17:04:07 -0300

    Add CORS to server.

--- ai/server/server.go
+++ ai/server/server.go
@@ -17,6 +17,14 @@ func handlePlay(player *ai.Player) http.HandlerFunc {
 	}
 
 	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
+		if r.Method == "OPTIONS" {
+			return
+		}
+
 		in := request{}
 		decoder := json.NewDecoder(r.Body)
 		err := decoder.Decode(&in)
