75681915910a80011d5f9f036ec8fa49b1c354a5
Tiago Madeira <tmadeira@gmail.com>
2020-03-08 13:17:05 -0300

    Use player 2 as default.

--- ai/cmd/server/main.go
+++ ai/cmd/server/main.go
@@ -8,7 +8,7 @@ import (
 
 var (
 	port      = flag.Int("port", 8080, "tcp port to listen")
-	playerID  = flag.Int("pid", 1, "ai player id (1 or 2)")
+	playerID  = flag.Int("pid", 2, "ai player id (1 or 2)")
 	strategy  = flag.String("strategy", "minimax", "ai strategy")
 	heuristic = flag.String("heuristic", "mindistance", "minimax heuristic to use")
 )
